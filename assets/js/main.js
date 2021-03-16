a454ceecc9763b24cdb47ba7d6c575c070d4f744
Giorgio <giorgio.pignattari@gmail.it>
2021-03-16 17:52:07 +0100

    inizio js, oggetto studente

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Consegna
+// Creare un oggetto che descriva uno studente,
+// con le seguenti proprietà: nome, cognome e età.
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+// Creare un array di oggetti di studenti.
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+// Dare la possibilità all’utente, attraverso 3 prompt(),
+// di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+$(document).ready(function(){
+// creo un oggetto studente e stampo
+var studente = {
+  nome: Giorgio,
+  cognome: Pignattari,
+  età: 26,
+}
+console.log(studente);
+
+})
